dea2392cb655e85723f47817da967bbf39be5635
Hibiki Kato <add.re.0828@gmail.com>
2021-03-17 01:12:05 +0900

    update:現状

--- cookstreak_project/mainapp/urls.py
+++ cookstreak_project/mainapp/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from django.conf.urls import url
 from .views import searchView
-from .views import index
 
 urlpatterns = [
     path('search/', searchView.as_view(),name='search'),
--- cookstreak_project/mainapp/views.py
+++ cookstreak_project/mainapp/views.py
@@ -4,11 +4,6 @@ from django.views.generic import TemplateView
 from .src.recipe.analyze_recipe import getRecipe
 from .src.recipe.search_recipesURL import getURL
 
-# Create your views here.
-def index(request):
-    return render(request, 'template.html')
-
-
 class searchView(TemplateView):
     def __init__(self):
         self.params={
@@ -18,9 +13,8 @@ class searchView(TemplateView):
         }
 
     def get(self,request):
-        return render(request,'templates/recipe/search_recipes.html',self.params)
+        return render(request,'templates/mainapp/recipe/search_recipes.html',self.params)
 
     def post(self,request):
         self.params['data']=getURL(request.POST['dish name'])
-        return render(request,'templates/recipe/search_recipes.html',self.params)
-
+        return render(request,'templates/mainapp/recipe/search_recipes.html',self.params)
